48ab5af9c6058b6a3b5a6cbabfc37eeba6d60b9f
Nikhil <nikhilbalamurugan@gmail.com>
2025-12-22 00:37:32 +0530

    update api call

--- frontend/script.js
+++ frontend/script.js
@@ -120,7 +120,7 @@ analyzeBtn.addEventListener('click', async () => {
         // ⚠️ CHANGE URL IF DEPLOYED
         // Local: 'http://127.0.0.1:8000/predict'
         // Render: 'https://your-app-name.onrender.com/predict'
-        const response = await fetch('http://127.0.0.1:8000/predict', {
+        const response = await fetch('https://dermaai-0h90.onrender.com/', {
             method: 'POST',
             body: formData
         });
